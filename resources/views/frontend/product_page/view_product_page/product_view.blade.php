1ebc9048f2221140e4b9ee7c3482dc9479223d90
LabibArefin123 <ah947777@gmail.com>
2026-09-15 00:20:51 +0600

    product view page broken down blade and fixed the wishlist button.

--- resources/views/frontend/product_page/view_product_page/product_view.blade.php
+++ resources/views/frontend/product_page/view_product_page/product_view.blade.php
@@ -5,9 +5,25 @@
 @section('content')
     @include('custom_layout.header')
     <link rel="stylesheet" href="{{ asset('css/custom_frontend/product_page/product_view/product_view_header.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/custom_frontend/product_page/product_view/product_view_gallery.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/custom_frontend/product_page/product_view/product_view_info.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/custom_frontend/product_page/product_view/product_view_details.css') }}">
+    <link rel="stylesheet" href="{{asset('css/custom_frontend/product_page/product_view/product_view_gallery/product_view_gallery_layout.css')}}">
+    <link rel="stylesheet" href="{{asset('css/custom_frontend/product_page/product_view/product_view_gallery/product_view_gallery_thumbnails.css')}}">
+    <link rel="stylesheet" href="{{asset('css/custom_frontend/product_page/product_view/product_view_gallery/product_view_gallery_main.css')}}">
+    <link rel="stylesheet" href="{{asset('css/custom_frontend/product_page/product_view/product_view_gallery/product_view_gallery_controls.css')}}">
+    <link rel="stylesheet" href="{{asset('css/custom_frontend/product_page/product_view/product_view_gallery/product_view_gallery_modal.css')}}">
+    {{-- Start of Product View Info CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/custom_frontend/product_page/product_view/product_view_info/product_view_meta.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom_frontend/product_page/product_view/product_view_info/product_view_options.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom_frontend/product_page/product_view/product_view_info/product_view_quantity.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom_frontend/product_page/product_view/product_view_info/product_view_actions.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom_frontend/product_page/product_view/product_view_info/product_view_benefits.css') }}">
+    {{-- End of Product View Info CSS --}}
+    {{-- Start of Product View Details CSS --}}
+    <link rel="stylesheet" href="{{asset('css/custom_frontend/product_page/product_view/product_view_details/product_view_details_tabs.css')}}">
+    <link rel="stylesheet" href="{{asset('css/custom_frontend/product_page/product_view/product_view_details/product_view_details_content.css')}}">
+    <link rel="stylesheet" href="{{asset('css/custom_frontend/product_page/product_view/product_view_details/product_view_details_specifications.css')}}">
+    <link rel="stylesheet" href="{{asset('css/custom_frontend/product_page/product_view/product_view_details/product_view_details_shipping.css')}}">
+    <link rel="stylesheet" href="{{asset('css/custom_frontend/product_page/product_view/product_view_details/product_view_details_reviews.css')}}">
+    {{-- End of Product View Details CSS --}}
     <link rel="stylesheet" href="{{ asset('css/custom_frontend/product_page/product_view/product_view_related.css') }}">
     <link rel="stylesheet" href="{{ asset('css/custom_frontend/product_page/product_view/product_view_responsive.css') }}">
 
